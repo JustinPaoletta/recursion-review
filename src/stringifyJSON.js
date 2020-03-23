30a85ca7a209c90d29e8fde89414836342b97151
Justin Paoletta <justinpaoletta@gmail.com>
2020-03-23 19:26:23 -0400

    almost done with stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,58 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // convert all elements that are not objects or arrays into string
+    //strings
+  var output = '';
+  if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }
+    //numbers
+    //booleans
+
+  if ((typeof obj === 'number') || (typeof obj === 'boolean')) {
+    return `${obj}`;
+  }
+    //null
+  if (obj === null) {
+    return `null`;
+  }
+    //undefined, functions
+  if ((typeof obj === 'undefined') || (typeof obj === 'function')) {
+    return undefined;
+  }
+
+  // if element is an array
+  if (typeof obj === 'object') {
+    if (Array.isArray(obj)) {
+      // return stringifyJSON on element
+      if (obj.length === 0) {
+        return `[]`;
+      }
+      let mapped = obj.map((piece) => {
+        if ((typeof piece === 'function') || (typeof piece === 'undefined')) {
+          return stringifyJSON(null);
+        } else {
+        return stringifyJSON(piece);
+        }
+      })
+      return '[' + mapped + ']';
+
+      // return output
+    }
+      //else if element is an object
+      var keys = Object.keys(obj)
+      if ((keys.length === 0) ||(obj[keys[i]] === undefined)){
+        return '{}'
+      }else {
+      for (var i = 0; i < keys.length; i++) {
+        return '{' + stringifyJSON(keys[i]) + ':' + stringifyJSON(obj[keys[i]]) + '}'
+      }
+
+        // return stringifyJSON on element
+    }
+
+
+  }
+  return output;
 };
