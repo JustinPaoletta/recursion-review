406f0a0d9e83cffe851e4d7a068b20f4d8a88798
Justin Paoletta <justinpaoletta@gmail.com>
2020-03-23 23:01:25 -0400

    testing pomander

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,27 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function (className) {
   // initialize an empty array to push elems to
   let elemsWithClass = [];
   // create a function that returns childnodes
-  let testClass = function(node) {
-    console.log (node.classList)
-    if(node.classList && node.classList.contains(className)){
+  let testClass = function (node) {
+    if (node.classList && node.classList.contains(className)) {
       elemsWithClass.push(node);
     }
-    if(node.hasChildNodes()){
+    if (node.hasChildNodes()) {
       node.childNodes.forEach((child) => {
         testClass(child);
-      })
+      });
     }
 
-  }
+  };
   // test for the classname on each node
-
   // push to array the elems that pass the test
-
   // if there are child nodes on the current elem run the function again on each child node
   testClass(document.body);
   // return the array
