62b3fecae6508b92e7a9e97f9b7cb2ffeff125f8
Justin Paoletta <justinpaoletta@gmail.com>
2020-03-23 21:31:10 -0400

    finished

---
+++ src/mytest.js
@@ -0,0 +1,4 @@
+var stringifyJSON = function (obj) {
+
+
+};
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,59 +3,67 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function (obj) {
   // convert all elements that are not objects or arrays into string
-    //strings
-  var output = '';
+  //strings
   if (typeof obj === 'string') {
     return `"${obj}"`;
   }
-    //numbers
-    //booleans
+  //numbers
+  //booleans
 
   if ((typeof obj === 'number') || (typeof obj === 'boolean')) {
     return `${obj}`;
   }
-    //null
+  //null
   if (obj === null) {
     return `null`;
   }
-    //undefined, functions
+  //undefined, functions
   if ((typeof obj === 'undefined') || (typeof obj === 'function')) {
     return undefined;
   }
 
   // if element is an array
-  if (typeof obj === 'object') {
-    if (Array.isArray(obj)) {
-      // return stringifyJSON on element
-      if (obj.length === 0) {
-        return `[]`;
-      }
-      let mapped = obj.map((piece) => {
-        if ((typeof piece === 'function') || (typeof piece === 'undefined')) {
-          return stringifyJSON(null);
-        } else {
+
+  if (typeof obj === 'object' && Array.isArray(obj)) {
+    // return stringifyJSON on element
+    if (obj.length === 0) {
+      return `[]`;
+    }
+    let mapped = obj.map((piece) => {
+      if ((typeof piece === 'function') || (typeof piece === 'undefined')) {
+        return stringifyJSON(null);
+      } else {
         return stringifyJSON(piece);
-        }
-      })
-      return '[' + mapped + ']';
-
-      // return output
-    }
-      //else if element is an object
-      var keys = Object.keys(obj)
-      if ((keys.length === 0) ||(obj[keys[i]] === undefined)){
-        return '{}'
-      }else {
-      for (var i = 0; i < keys.length; i++) {
-        return '{' + stringifyJSON(keys[i]) + ':' + stringifyJSON(obj[keys[i]]) + '}'
       }
-
-        // return stringifyJSON on element
-    }
-
+    })
+    return '[' + mapped + ']';
 
   }
-  return output;
+
+  //else if element is an object
+
+  if (typeof obj === 'object'){
+
+    let objArr = [];
+
+    let myKeys = Object.keys(obj);
+
+    myKeys.forEach((key) => {
+      if( typeof obj[key] === 'function' || obj[key] === undefined){
+        delete obj[key];
+      }
+    });
+
+    if(myKeys.length === 0){
+      return '{}'
+    }else{
+      for(let key in obj){
+        objArr.push(`${stringifyJSON(key)}:${stringifyJSON(obj[key])}`);
+      }
+      return `{${objArr}}`
+    }
+  }
+
 };
